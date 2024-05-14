04628cd1d8a217cc13674bc2b5739a9b944578e7
jarudech <jarudech013@gmail.com>
2024-05-14 13:30:47 +0700

    did exercise 1-6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,12 @@
 // Start coding here
+let add = (a, b) => a + b;
+console.log(add(10, 5));
+
+let substract = (a, b) => a - b;
+console.log(substract(10, 5));
+
+let multiply = (a, b) => a * b;
+console.log(multiply(10, 5));
+
+let divide = (a, b) => a / b;
+console.log(divide(10, 5));
